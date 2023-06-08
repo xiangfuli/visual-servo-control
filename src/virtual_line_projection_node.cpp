a33835168eb4f3e1c905b066f00177031e680147
xiangfuli96 <xiangfuli96@gmail.com>
2023-06-08 12:20:47 -0400

    Temporarily working

--- src/virtual_line_projection_node.cpp
+++ src/virtual_line_projection_node.cpp
@@ -6,6 +6,7 @@
 #include <ros/console.h>
 #include "sensor_msgs/Image.h"
 #include "sensor_msgs/CameraInfo.h"
+#include "geometry_msgs/PoseStamped.h"
 #include "nav_msgs/Odometry.h"
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
@@ -30,8 +31,18 @@ Eigen::Vector3f vehicle_position;
 Eigen::Vector4f vehicle_pose;
 Eigen::Matrix<float, 3, 3> K_matrix;
 
-float rho, last_rho = -10;
-float distance_to_the_camera_center = 0.0f, distance_to_the_line = 0.0f;
+float rho, last_rho, pitch_radiant, last_pitch_radiant;
+float depth_to_the_line, last_depth_to_the_line;
+float pixel_distance_to_the_camera_center, last_pixel_distance_to_the_camera_center;
+float velocity_filtered = 0;
+float pitch_velocity;
+ros::Time last_ts_get_depth_image = ros::Time(1.0), ts_get_depth_image;
+
+bool ready_to_compute_velocity_y = false;
+bool already_prejected_lines = false;
+
+// publish the virtual line's properties: rho and theta
+vision_lines::line virtual_line_msg;
 
 bool already_received_odom = false;
 
@@ -40,41 +51,96 @@ void vehicle_odom_callback(nav_msgs::Odometry odom) {
   vehicle_pose << odom.pose.pose.orientation.w, odom.pose.pose.orientation.x, 
                   // in the simulation, the pitch angle is incorrect
                   odom.pose.pose.orientation.y, odom.pose.pose.orientation.z;
+  pitch_velocity = odom.twist.twist.angular.y;
   already_received_odom = true;  
 }
 
-void depth_image_callback(sensor_msgs::Image depth_image) {
-  // get the distance to the center of the camera
-  std::vector<uchar>::iterator it = depth_image.data.begin();
-  uchar *pointer = (uchar *)&(*it);
-  distance_to_the_camera_center = *(float *)(pointer + depth_image.step * depth_image.height / 2 + depth_image.step / 2);
-  
-  // ROS_INFO_THROTTLE(1, "Get the distance to the camera center pixel: %f.", distance_to_the_camera_center);
-
+void get_the_depth_to_the_line(Eigen::Vector2i virtual_line_left_pixel,
+  Eigen::Vector2i virtual_line_right_pixel,
+  int image_width,
+  int image_height,
+  int image_row_step,
+  uchar *pointer,
+  Eigen::Matrix<float, 3, 3> K_matrix,
+  float &pixel_distance_to_camera_center,
+  float &depth_to_the_line) {
   // get the distance to the detected line
   // first get the pixel point in the middle of the image
   float A = virtual_line_right_pixel.y() - virtual_line_left_pixel.y();
   float B = virtual_line_right_pixel.x() - virtual_line_left_pixel.x();
   float C = virtual_line_left_pixel.x() * virtual_line_right_pixel.y() 
             - virtual_line_right_pixel.x() *  virtual_line_left_pixel.y();
-  int line_pixel_length_to_camera_center = (A * depth_image.width / 2 + B * depth_image.height / 2  + C) / sqrt(A*A + B*B);
-  if (abs(line_pixel_length_to_camera_center) > depth_image.height / 2) {
-    ROS_ERROR_THROTTLE(1, "Line is no longer in vision. line position: %d", line_pixel_length_to_camera_center);
+  pixel_distance_to_camera_center = (virtual_line_right_pixel.y() + virtual_line_left_pixel.y()) / 2.0 - image_height/2;
+  if (abs(pixel_distance_to_camera_center) > (image_height / 2)) {
+    ROS_ERROR_THROTTLE(1, "Line is no longer in vision. line position: %f", pixel_distance_to_camera_center);
     return;
   }
-  distance_to_the_line = *(float *)(pointer + depth_image.step * int(K_matrix(1, 2) - line_pixel_length_to_camera_center) + depth_image.step / 2);
-  // ROS_INFO_THROTTLE(1, "Get the distance to the line: %f.", distance_to_the_line);
 
-  // calculate the rho in the virtual vertical image plane
-  // first get the pitch angle
-  Eigen::Vector3f rpy;
-  SO3Utils::quaternion_2_RPY(vehicle_pose, rpy);
-  float pitch_radiant = abs(rpy.y());
+  depth_to_the_line = *(float *)(pointer + image_row_step * int(K_matrix(1, 2) - pixel_distance_to_camera_center) + image_row_step / 2);
+  // ROS_INFO_THROTTLE(1, "Get the distance to the line: %f.", depth_to_the_line);
+}
 
-  rho = (distance_to_the_camera_center * sin(pitch_radiant) + 
-          line_pixel_length_to_camera_center / cos(pitch_radiant) / K_matrix(1, 1) * distance_to_the_line) 
-        / distance_to_the_line * K_matrix(1, 1);
-  rho /= K_matrix(1, 1);
+/**
+ * @brief  This function is used to calculate the velocity of the vehicle in z direction
+ * @note   
+ * @param  depth_image: 
+ * @retval None
+ */
+void depth_image_callback(sensor_msgs::Image depth_image) {
+  if (!already_received_odom || !already_prejected_lines) {
+    return;
+  }
+
+  // get the distance to the center of the camera
+  std::vector<uchar>::iterator it = depth_image.data.begin();
+  uchar *pointer = (uchar *)&(*it);
+  // distance_to_the_camera_center = *(float *)(pointer + depth_image.step * depth_image.height / 2 + depth_image.step / 2);
+  // ROS_INFO_THROTTLE(1, "Get the distance to the camera center pixel: %f.", distance_to_the_camera_center);
+
+  get_the_depth_to_the_line(virtual_line_left_pixel, virtual_line_right_pixel, 
+    depth_image.width, depth_image.height, depth_image.step, pointer, K_matrix, pixel_distance_to_the_camera_center, depth_to_the_line);
+
+  // Get the pitch angle
+  // Eigen::Vector3f rpy;
+  // SO3Utils::quaternion_2_RPY(vehicle_pose, rpy);
+  // pitch_radiant = rpy.y();
+
+  // initialize all the required variables
+  if (!ready_to_compute_velocity_y) {
+    last_ts_get_depth_image = depth_image.header.stamp;
+    last_pitch_radiant = pitch_radiant;
+    last_depth_to_the_line = depth_to_the_line;
+    last_pixel_distance_to_the_camera_center = pixel_distance_to_the_camera_center;
+    ready_to_compute_velocity_y = true;
+    return;
+  }
+
+  // depending on the velocity of the line pixels motion, we can directly estimate the velocity of the vehicle
+
+  // compute the required velocity
+  ts_get_depth_image = depth_image.header.stamp;
+  float dt = ts_get_depth_image.toSec() - last_ts_get_depth_image.toSec();
+  // float pitch_velocity = (pitch_radiant - last_pitch_radiant) / dt;
+  float depth_velocity = (depth_to_the_line - last_depth_to_the_line) / dt;
+  float pixel_velocity = (pixel_distance_to_the_camera_center - last_pixel_distance_to_the_camera_center) / dt;
+
+  float fy = K_matrix(1, 1);
+  float position_y = pixel_distance_to_the_camera_center / fy * depth_to_the_line;
+  float velocity = pixel_velocity * depth_to_the_line / fy - depth_velocity / depth_to_the_line * position_y;
+
+  velocity = -velocity + (-pitch_velocity) * depth_to_the_line;
+
+  // transform velocity from image frame to vehicle frame
+  velocity = -velocity;
+
+  // use low-pass filter to get the state velocity
+  velocity_filtered = 0.2f * velocity_filtered + 0.8f * velocity;
+  virtual_line_msg.Bottom.x = velocity_filtered;
+
+  last_depth_to_the_line = depth_to_the_line;
+  // last_pitch_radiant = pitch_radiant;
+  last_pixel_distance_to_the_camera_center = pixel_distance_to_the_camera_center;
+  last_ts_get_depth_image = depth_image.header.stamp;
 }
 
 void rgb_callback(sensor_msgs::Image rgb_image) {
@@ -97,23 +163,19 @@ void rgb_callback(sensor_msgs::Image rgb_image) {
     virtual_line_start_point, 
     virtual_line_end_point,
     virtual_line_left_pixel, virtual_line_right_pixel);
-
-  // publish the virtual line's properties: rho and theta
-  vision_lines::line virtual_line_msg;
   
   float theta = atan2(virtual_line_right_pixel.y() - virtual_line_left_pixel.y(),
     virtual_line_right_pixel.x() - virtual_line_left_pixel.x());
   
-  // float A = virtual_line_right_pixel.y() - virtual_line_left_pixel.y();
-  // float B = virtual_line_right_pixel.x() - virtual_line_left_pixel.x();
-  // float C = virtual_line_left_pixel.x() * virtual_line_right_pixel.y() 
-  //           - virtual_line_right_pixel.x() *  virtual_line_left_pixel.y();
+  float A = virtual_line_right_pixel.y() - virtual_line_left_pixel.y();
+  float B = virtual_line_right_pixel.x() - virtual_line_left_pixel.x();
+  float C = virtual_line_left_pixel.x() * virtual_line_right_pixel.y() 
+            - virtual_line_right_pixel.x() *  virtual_line_left_pixel.y();
 
-  // // publish the velocity of the property rho
-  // // first, detect the line
-  // rho = (A * rgb_image.width / 2 + B * rgb_image.height / 2  + C) / sqrt(A*A + B*B) 
-  //                             / rgb_image.height * 2;
-
+  // publish the velocity of the property rho
+  // first, detect the line
+  rho = (A * rgb_image.width / 2 + B * rgb_image.height / 2  + C) / sqrt(A*A + B*B) 
+                              / rgb_image.height * 2;
 
   // here we assume our vehicle body is level
   virtual_line_msg.header.stamp = ros::Time::now();
@@ -123,7 +185,7 @@ void rgb_callback(sensor_msgs::Image rgb_image) {
   if (last_rho == -10) {
     virtual_line_msg.Bottom.x = 0;
   } else {
-    virtual_line_msg.Bottom.x = (last_rho - rho);
+    // virtual_line_msg.Bottom.x = (last_rho - rho);
   }
   last_rho = rho;
 
@@ -139,6 +201,8 @@ void rgb_callback(sensor_msgs::Image rgb_image) {
   img_bridge.toImageMsg(image_msg);
 
   rgb_virtual_image_pub.publish(image_msg);
+
+  already_prejected_lines = true;
 }
 
 int main(int argc, char** argv) {
